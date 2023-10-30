2b47e1886705decadc5369ae34398321f512253d
Ismaeel <ismaeeldildar98@gmail.com>
2023-10-30 12:00:00 +0000

    Add AddNewProduct.jsx

---
+++ src/components/AddNewProduct.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useAddNewProductMutation } from '../app/service/apiData'
+
+const AddNewProduct = () => {
+    const [AddNewProduct,{data,isError,isLoading}] = useAddNewProductMutation()
+    if(isError){
+        alert("we got an error")
+    }
+    if(isLoading){
+        return <h1>Loading . . .. . </h1>
+    }
+
+    const handleAddProduct = async () =>{
+        try {
+            const newProductData = {
+                id: 1,
+                title: "Amazing Sound Bar",
+                description: "This is one of the best soundbar in the market"
+            }
+            
+            await AddNewProduct(newProductData)
+
+        } catch (error) {
+            alert("we got an error",error)
+        }
+    }
+
+  return (
+    <>
+    <button onClick={handleAddProduct} disabled={isLoading}>Add new Product</button>
+        <h1>{data?.brand}</h1>
+        <h2>{data?.id}</h2>
+        <p>{data?.description}</p>
+    </>
+  )
+}
+
+export default AddNewProduct
