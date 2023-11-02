79287288d8e9f2359b4110e32b6370fb292cc3d1
Ismaeel <ismaeeldildar98@gmail.com>
2023-11-02 12:00:00 +0000

    Add SpecificProduct.jsx

---
+++ src/components/SpecificProduct.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useGetProductByIdQuery } from '../app/service/apiData'
+
+const SpecificProduct = () => {
+    const {data,isError,isLoading} = useGetProductByIdQuery(2)
+    if(isError){
+        alert("we got an error")
+    }
+    if(isLoading){
+        return <h1>Loading . . .. . </h1>
+    }
+    console.log(data)
+  return (
+    <>
+    <div>
+        <h1>{data?.brand}</h1>
+        <h2>{data?.id}</h2>
+        <p>{data?.description}</p>
+
+    </div>
+    </>
+  )
+}
+
+export default SpecificProduct
